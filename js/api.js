b6648bc9bebfae15e72b1881b73630b44e8ec40e
김건우 <kgw010119@gmail.com>
2021-07-25 08:14:53 +0000

    정확한 날짜 계산 기능 추가

--- js/api.js
+++ js/api.js
@@ -2,10 +2,10 @@ function comma(str) {
     str = String(str);
     return str.replace(/(\d)(?=(?:\d{3})+(?!\d))/g, '$1,');
 }
-var coin_volume = 3500;
-var stock_volume = 8;
-var exchange_rate;
-var trx;
+const coin_volume = 3500;
+const stock_volume = 8;
+let exchange_rate;
+let trx;
 
 $(function() {
     $.ajax({
@@ -24,23 +24,27 @@ $(function() {
                 var j = 0;
                 for (var i = 0; i < data.Count; i++) {
                     if (item[i].countvalue != 0&&item[i].commitvalue != 0) {
-                        arr[j][0] = item[i].timevalue;
-                        arr[j][1] = item[i].countvalue;
+						arr[j][0] = item[i].timevalue;
+						arr[j][1] = item[i].countvalue;
 						arr[j][2] = item[i].commitvalue;
-                        j++;
+						j++;
                     }
                 }
                 arr.sort(function(a, b) {
                     return b[0] - a[0];
                 });
-                var baekjoon_rate = arr[0][1] - arr[1][1];
+				if(Math.floor(arr[0][0]/10000)!=Math.floor(arr[1][0]/10000)) arr[1][2]=0;
+				
+				var baekjoon_rate = arr[0][1] - arr[1][1];
 				var github_commit = arr[0][2] - arr[1][2];
-                document.getElementById('baekjoon').innerHTML = arr[0][1] + '(+' + baekjoon_rate + ')';
+				
+				document.getElementById('baekjoon').innerHTML = arr[0][1] + '(+' + baekjoon_rate + ')';
 				document.getElementById('github').innerHTML = arr[0][2] + '(+' + github_commit + ')';
-                if (baekjoon_rate == 0) document.getElementById('baekjoon').style.color = "#FF0000";
-                else document.getElementById('baekjoon').style.color = "#01DF01";
+				
+				if (baekjoon_rate == 0) document.getElementById('baekjoon').style.color = "#FF0000";
+				else document.getElementById('baekjoon').style.color = "#01DF01";
 				if (github_commit == 0) document.getElementById('github').style.color = "#FF0000";
-                else document.getElementById('github').style.color = "#01DF01";
+				else document.getElementById('github').style.color = "#01DF01";
             })
         }
     })
@@ -71,10 +75,10 @@ $(function() {
         url: "https://9wl9vr5c1l.execute-api.ap-northeast-2.amazonaws.com/default/Crawling-Example",
         dataType: "json",
         success: function(data) {
-            var stock_price = Math.floor(data[0] * stock_volume * exchange_rate);
+            let stock_price = Math.floor(data[0] * stock_volume * exchange_rate);
             document.getElementById('stock').innerHTML = comma(stock_price);
             document.getElementById('total').innerHTML = comma(stock_price + trx);
-            var goal = (stock_price + trx) / 100000;
+            let goal = (stock_price + trx) / 100000;
             document.getElementsByTagName('progress')[0].value = goal.toFixed(2);
             document.getElementsByTagName('b')[0].innerText = ' ' + goal.toFixed(2) + '%';
         }
@@ -82,6 +86,7 @@ $(function() {
 })
 
 function time() {
-    var time = new Date();
+    let time = new Date();
     document.getElementById("now").innerHTML = time.getFullYear() + "/" + (time.getMonth() + 1) + "/" + time.getDate() + "/" + ("0" + time.getHours()).slice(-2) + ":" + ("0" + time.getMinutes()).slice(-2) + ":" + ("0" + time.getSeconds()).slice(-2);
+	document.getElementById("year").innerHTML = time.getFullYear();
 }
